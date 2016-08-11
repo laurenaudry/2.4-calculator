b3a8f7e7ce62fad5efe61d9afe687f056017ac34
Lauren Johnson <laurenaudry1995@gmail.com>
2016-08-11 16:37:36 -0500

    started javascript for calculator

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+  var buttons = document.querySelectorAll(".button"),
+      result = document.querySelector('.result')[0],
+      clear = document.getElementsByClassName('clear')[0];
+)};
+
+  button.forEach(function(numButton){
+    numButton.addEventListener("click", function(e){
+      
+  }
