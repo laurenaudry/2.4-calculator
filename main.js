ccbcd0c9f1f300ca97fc7c61f86cf052e12c5993
Lauren Johnson <laurenaudry1995@gmail.com>
2016-08-12 15:17:21 -0500

    finished loops to make working calculator.

--- main.js
+++ main.js
@@ -2,17 +2,28 @@ document.addEventListener("DOMContentLoaded", function(){
   var button = document.querySelectorAll(".button");
   var result = document.querySelector(".result");
   var clear = document.querySelector(".clear");
-
+  var string = "";
 
 
  button.forEach(function(numButton){
    numButton.addEventListener("click", function(e){
      var number = e.target.textContent;
-     result.textContent += number;
+     console.log(e.target);
+     if (e.target.textContent === "=") {
+      result.textContent = eval(string);
+    } else {
+      result.textContent += number;
+      string += number;
+    }
    });
  });
 
  clear.addEventListener("click", function(e){
    result.textContent = "";
+   string = "";
   });
+for (var i = 0; i < button.length; i++) {
+  button[i]
+}
+
 });
